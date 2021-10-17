707c8a92e407f678ee3b86be2d6ec2939c685744
Viktor Tholén <viktor.tholen@gmail.com>
2021-10-17 22:24:05 +0200

    fixed deploy issues

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/portfolio-website' + process.env.CI_PROJECT_NAME + '/'
-      : '/'
-  }
+    publicPath: "/portfolio/"
+}
