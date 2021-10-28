2691db3a484c92c6ae0885b529b364ef82b57ccd
Viktor Tholén <viktor.tholen@gmail.com>
2021-10-28 18:24:58 +0200

    grammar

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,7 +3,7 @@
     <div class= "contact">
         <p class=title>CONTACT</p>
         <p>Mail: viktor.tholen@gmail.com</p>
-        <p>Telefon: +46702980745</p>
+        <p>Phone: +46702980745</p>
         <p>Github: <a href="https://github.com/viktortholen">viktortholen</a></p>
         <p>Linkedin: <a href="https://www.linkedin.com/in/viktor-tholen">Viktor Tholén</a></p>
     </div>
